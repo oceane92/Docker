4a487425473e1acf5d61c90a8806176f4325e538
oceane92 <34437842+oceane92@users.noreply.github.com>
2020-02-12 17:07:54 +0100

    Exo  1

--- server1.js
+++ server1.js
@@ -1,9 +1,7 @@
-var http = require("http");
+var http = require('http'),
+io = require('socket.io');
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
+var server = http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
@@ -12,3 +10,17 @@ http.createServer(function (request, response) {
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:5372/');
+
+// Envoi de la requête connexion
+io = io.listen(server);
+io.sockets.on('connection', function(socket){
+  console.log('Client connected.');
+  socket.on('disconnect', function() {
+  console.log('Client disconnected.');
+  });
+});
+
+var socket = io.connect('http://127.0.0.1:4567', {reconnect: true});
+socket.on('connect', function(socket) { 
+   console.log('Ping !');
+});
